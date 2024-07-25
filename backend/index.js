ec4df406be26b3c90beded97a9c28afc7845d016
Muhammed Zain <muhammedzain2256@gmail.com>
2024-07-25 22:58:45 +0500

    Backend setup

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,13 @@ import cors from 'cors';
 
 const app = express();
 
+app.get("/home", (req,res) => {
+    return res.status(200).json({
+        message: "I am coming from backend",
+        success: true
+    })
+})
+
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -12,6 +19,7 @@ const corsOption = {
     origin: 'http://localhost:5173/',
     credentials: true
 }
+
 app.use(cors(corsOption));
 
 const PORT = 3000;
